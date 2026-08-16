5441c6e2f24082bd2a6a4bf558029b16cea14218
nirabu6-rgb <nirabu6@gmail.com>
2026-08-16 16:52:51 +0300

    WeightStock: cloud sync, 8 languages, scale connections, kg/lbs, consumption forecast, orders v2

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-self.addEventListener('install', function(e) { self.skipWaiting(); });
-self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', function(e) {});
+PLACEHOLDER
